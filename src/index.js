fbedd580ccb337aad61840c3d1d57fca9aa95bae
Thomas Lind <thomas.lind@tlcgolfit.se>
2021-11-22 16:16:48 +0100

    remove

--- src/index.js
+++ src/index.js
@@ -7,20 +7,9 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  /*
-  <Auth0Provider
-    domain="https://login.ref.ug.kth.se/adfs/oauth2"
-    clientId="c977e3cc-a6bf-4843-bb7f-6fd0c57cfe60"
-    redirectUri={'https://apps-ref.lib.kth.se/mrbs'}
-    //redirectUri={window.location.origin}
-  >
-  */
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  /*
-  </Auth0Provider>
-  */
   ,
   document.getElementById('root')
 );
